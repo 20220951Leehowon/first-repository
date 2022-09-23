0f8c74d8190bb0e934d52cbc75419d54b1775397
20220951Leehowon <hdkam0323@gmail.com>
2022-09-23 16:26:53 +0900

    대충 실습자료

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import max_profit as mp
 import prime_number as pn
-import random
 
 if __name__ == '__main__':
     pn.is_prime_number(1)
@@ -10,8 +9,3 @@ if __name__ == '__main__':
     pn.is_prime_number(45)
     pn.is_prime_number(97)
 
-<<<<<<< HEAD
-a = random(1,10)
-=======
-a = random(1,10)
->>>>>>> 5edbc695f53d54002f508aa2ac3a9ba7928dabd6
